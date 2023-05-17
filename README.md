4bc36187badd5e8ef4f788bfe382049ef16a7727
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-05-17 17:06:28 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Practice_15.05.2023
+Практика: map
+
+<a href="https://gist.github.com/SlavikArt/c8336f61f8226ccb20fe6879f8a787cf">Gist - страница со всеми кодами проектов</a>
+
+* [main](main)
+
+<p align="center">
+    <h2>Main</h2>
+    <p></p>
+    <img src="images/main.png">
+</p>
---
+++ main/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+
+class Student
+{
+    std::string name;
+    double age;
+public:
+    Student(std::string name, double age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+
+    bool operator < (const Student& s) const
+    {
+        return this->name < s.name;
+    }
+};
+
+int main()
+{
+    std::map<Student, double> dict;
+    
+    Student students[] = {
+        { "Vasya", 18 },
+        { "Petya", 17.5 },
+        { "Oleksandr", 16.5 },
+        { "Yevheniia", 18 },
+        { "Anastasiia", 16 },
+        { "Dmytro", 17 }
+    };
+    double grades[] = { 10.2, 12.0, 9.7, 8.0, 10.5, 11.6 };
+
+    for (int i = 0; i < 6; i++)
+    {
+        Student student = students[i];
+        double grade = grades[i];
+
+        dict[student] = grade;
+    }
+
+    auto it = dict.find({ "Petya", 17.5 });
+
+    if (it != dict.end())
+        std::cout << "Found: " << it->second << "\n";
+    else
+        std::cout << "Not Found :(\n";
+}
